188dc3b85faf6ff1cf1708250276695ca0de4493
Cordelia Clay <cordelia.clay@new.ox.ac.uk>
2024-11-16 15:46:22 +0000

    Assignment commit

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,7 @@
 #Script to plot data and model
 
-
+install.packages(ggplot)
+library("ggplot2")
 
 # Define the logistic growth function
 logistic_fun <- function(t) {
@@ -9,19 +10,19 @@ logistic_fun <- function(t) {
 }
 
 # Set parameters for the logistic model
-N0 <- 879       # Initial population (from excel file)
-r <- 0.0100086        # Growth rate (0.0100086, taken from the initial linear model)
-K <- 5.979e+10       # Carrying capacity, from the second linear model, gives you the intercept, which is the number of individuals at carrying capacity
+N0 <- exp(6.888e+00)      # Initial population size (From Scenario 1)
+r <- 1.002e-02       # Growth rate (From Scenario 1)
+K <- 5.979e+10       # Carrying capacity (From Scenario 2)
 
 # Plot the data and the logistic model
-ggplot(aes(x = t, y = N), data = growth_data) +  # Replace 't' and 'N' with your actual column names
+ggplot(aes(x = t, y = N), data = growth_data) +  
   geom_function(fun = logistic_fun, colour = "red") +
   geom_point() +
-  xlab("Time") +
-  ylab("Population")
+  scale_y_continuous(trans = 'log10') + 
+  xlab("Time (Minutes)") +
+  ylab("Population Size (N)")
 
 
-# Uncomment the following line if you want to use a log scale on the y-axis
-scale_y_continuous(trans = 'log10')
+
 
 
